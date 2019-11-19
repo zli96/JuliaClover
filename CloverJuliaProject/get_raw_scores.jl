f9b92f5b1d3d1b2ddd6114bfa86aea1a633ece68
justdunno <fountain_woo@outlook.com>
2019-11-19 14:05:48 -0500

    Update get_raw_scores.jl

--- CloverJuliaProject/get_raw_scores.jl
+++ CloverJuliaProject/get_raw_scores.jl
@@ -26,8 +26,6 @@ results = Array{result, 1}(undef, length(doubleStrandMotifs))
 motnum= 1 #temp
 seqnum= 1 #temp
 hit_thresh=6 #temp
-tot_score=0
-
 base_probs=[0.3 0.1 0.5 0.1]
 tot_score=scan_seq(sequence[1], doubleStrandMotifs[1], base_probs, seqnum, motnum, hit_thresh)
 print("tot_score:", tot_score,"\n")
